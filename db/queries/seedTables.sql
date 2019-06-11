c5d0f038f49d9b04492a2dd833def83b34be5ea7
alecjmaly <32174181+alecjmaly@users.noreply.github.com>
2019-06-10 20:05:31 -0400

    Update seedTables.sql

--- db/queries/seedTables.sql
+++ db/queries/seedTables.sql
@@ -1,7 +1,7 @@
 INSERT INTO Address(Street_no, Street, City, State, Zip) VALUES 
-  (1023, 'E Huron River Dr.', 'Van Buren', 'MI', 48111),
-  (12305, 'Westlake Cir', 'Van Buren', 'MI', 48111),
-  (1005, 'E Huron River Dr.', 'Van Buren', 'MI', 48111); ---
+  (1023, 'E. Made Up St.', 'Detroit', 'MI', 48126),
+  (12305, 'Westlake Dr.', 'Canton', 'MI', 48188),
+  (1005, 'Fort St.', 'Allen Park', 'MI', 48101); ---
   
 INSERT INTO Credit_Card(Credit_card_num, Credit_card_expMon, Credit_card_expYear) VALUES
   (123456789123123, 01, 2022),
@@ -11,15 +11,15 @@ INSERT INTO Credit_Card(Credit_card_num, Credit_card_expMon, Credit_card_expYear
 
 
 INSERT INTO Person(First_name, Last_name, Address_id) VALUES
-  ('Alec', 'Maly', 1),
-  ('Olga', 'Van Bolt', 1),
+  ('Tom', 'Tomberino', 1),
+  ('Olivia', 'Peterson', 1),
   ('Fred', 'Mullington', 3),
-  ('Ella', 'Maly', 2); ---
+  ('Mary', 'Smith', 2); ---
 
 INSERT INTO Vehicle(VIN_no, License_Plate_No, Make, Model, Year, Color) VALUES 
   (2353, 'sdnf32', 'Chevrolet', 'Camero', 2017, 'Red'),
-  (1242, 'sdf3vv', 'Chevrolet', 'Camero', 2017, 'Red'),
-  (16123, 'sda332', 'Chevrolet', 'Camero', 2017, 'Red'); ---
+  (1242, 'sdf3vv', 'Ford', 'Taurus', 2015, 'White'),
+  (16123, 'sda332', 'Buick ', 'LeSabre', 2000, 'Green'); ---
 
 
 
